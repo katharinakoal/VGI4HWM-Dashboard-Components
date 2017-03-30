f70381290add5e4a43ff6187e8ba1ef6a8880ff4
Katharina Koal <k.koal@student.htw-berlin.de>
2017-03-30 12:24:23 +0200

    added category group stacking to time chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 import * as dc from 'dc';
 import * as L from 'leaflet';
@@ -25,62 +25,93 @@ export class DashboardComponent implements OnInit {
 
   private _timeChart: dc.BarChart;
 
-  constructor(private _dataService: DashboardDataService, private _mapService: DashboardMapService) {}
+  constructor(private _dataService: DashboardDataService, private _mapService: DashboardMapService) { }
 
   ngOnInit(): void {
 
     this._mapService.createMap();
     this._dataService.getData()
-      .subscribe( response => this.receiveData(response),
-                error => this.errorMessage = error
-    );
+      .subscribe(response => this.receiveData(response),
+      error => this.errorMessage = error
+      );
 
   }
 
-  private receiveData( mediaData: IMediaData[] ): void {
+  private receiveData(mediaData: IMediaData[]): void {
 
     const fullDateFormat = d3.time.format.iso;
 
-    for ( const mediaElement of mediaData ){
+    for (const mediaElement of mediaData) {
       (<IMediaData>mediaElement).date = d3.time.format.iso.parse(mediaElement.timestamp);
     }
 
     const cf = crossfilter(mediaData as IMediaData[]);
 
-    const allDim = cf.dimension( (d: IMediaData) => d );
-    const dateDim = cf.dimension( (d: IMediaData) => d.date );
+    const allDim = cf.dimension((d: IMediaData) => d);
+    const dateDim = cf.dimension((d: IMediaData) => d.date);
     const categoryDim = cf.dimension((d: IMediaData) => {
       d.category.valueOf = () => d.category.id;
       return d.category;
     });
+    const dateGroup = dateDim.group();
+    const categoryGroup = categoryDim.group();
 
-    for ( const group of categoryDim.group().all() ){
-      console.log(group.key.longname);
+    const categoryCounts = {};
+
+    for (const group of categoryGroup.all()) {
+      categoryCounts[group.key.shortname] = 0;
       let c = categoryDim.filter(group.key);
-      console.log(c.top(Infinity));
     }
 
     /** marked for refactoring */
     categoryDim.filterAll();
 
+    const categoryByDate = dateDim.group().reduce(
+      (p, v) => {
+        p[v.category.shortname]++;
+        return p;
+      },
+      (p, v) => {
+        p[v.category.shortname]--;
+        return p;
+      },
+      () => Object.assign({}, categoryCounts)
+    );
+
     this._timeChart = dc.barChart('#time-chart');
 
     this._timeChart.on('renderlet', () => this._mapService.updateMarker(allDim.top(Infinity)))
-        .height(300)
-        .margins({top: 20, right: 0, bottom: 20, left: 25})
-        .dimension(dateDim)
-        .group(dateDim.group())
-        .barPadding(2)
-        .centerBar(true)
-        .elasticY(true)
-        .x(d3.time.scale().domain([
-          d3.time.day.offset(d3.min(mediaData, (d: IMediaData) => d.date ), -1),
-          d3.time.day.offset(d3.max(mediaData, (d: IMediaData) => d.date ), 1)
-          ]))
-        .xUnits(function(){ return 20; });
+      .height(300)
+      .margins({ top: 20, right: 0, bottom: 20, left: 25 })
+      .dimension(dateDim)
+      .barPadding(2)
+      .centerBar(true)
+      .elasticY(true)
+      .x(d3.time.scale().domain([
+        d3.time.day.offset(d3.min(mediaData, (d: IMediaData) => d.date), -1),
+        d3.time.day.offset(d3.max(mediaData, (d: IMediaData) => d.date), 1)
+      ]))
+      .xUnits(function () { return 20; });
+
+    categoryGroup.all().forEach((group, index) => {
+
+      if (!!index) {
+        this._timeChart.stack(categoryByDate, group.key.shortname, function (d) {
+          return d.value[group.key.shortname];
+        });
+      } else {
+        this._timeChart.group(categoryByDate).valueAccessor(function (d) {
+          return d.value[group.key.shortname];
+        });
+      }
+
+    });
 
     dc.renderAll();
-
-
   }
+
+  public onResize(event): void {
+    this._timeChart.render();
+  }
+
 }
